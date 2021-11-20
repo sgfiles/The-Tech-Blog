167ab4257281f4ed8c7965b6e653bd55d381da72
Sheralyn Files <gired0329@yahoo.com>
2021-11-19 21:49:03 -0500

    fixing models

--- Main/controllers/api/userRoutes.js
+++ Main/controllers/api/userRoutes.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { userName: req.body.email } });
+    const userData = await User.findOne({ where: { userName: req.body.username } });
 
     if (!userData) {
       res
--- Main/models/index.js
+++ Main/models/index.js
@@ -1,14 +1,20 @@
 const User = require('./User');
 const Post = require('./Post');
-const comment = require('./comments')
+const Comment = require('./Comment')
 
-User.hasMany(Post, {
+Post.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Comment, Post };
