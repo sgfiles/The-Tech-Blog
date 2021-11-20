cf40007adbc0e26a6fa9bcf0b4a2fbc9dc5c7660
Sheralyn Files <gired0329@yahoo.com>
2021-11-19 21:02:40 -0500

    revising comment routes

--- Main/controllers/api/commentRoutes.js
+++ Main/controllers/api/commentRoutes.js
@@ -2,46 +2,17 @@ const router = require('express').Router();
 const { Comment} = require('../../models')
 const withAuth = require('../../utils/auth')
 
-// router.get('/', (req,res) => {
-//     Comment.findAll()
-//     .then(dbCommentData => res.json(dbCommentData))
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
 
-router.post('/', withAuth, (req, res) => {
-    Comment.create({
-        content: req.body.content,
-        user_id: req.session.user_id,
-        post_id: req.body.post_id
-    })
-
-    .then(commentData => res.json(commentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
-
-});
-
-// if (req.session) {
-//     Comment.create({
-//         comment_text: req.body.comment_text,
-//         post_id: req.body.post_id,
-//         user_id: req.session.user_id,
-//     })
-//     .then(dbCommentData => res.json(dbCommentData))
-//     .catch(err => {
-//         console.log(err);
-//         res.status(400).json(err);
-//     })
-// }
-
-// To Do
-// router.put
-// router.delete
-
+router.post('/', withAuth, async (req, res) => {
+   try {
+       const newComment = await Comment.create({
+           ...req.body,
+           userId: req.session.userId,
+       });
+       res.json(newComment);
+   } catch(error) {
+       res.status(500).json(error);
+   }
+ })
 
 module.exports = router;
