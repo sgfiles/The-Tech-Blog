8223572a0e9ca79313f4d52692fca0cd3ac57924
Sheralyn Files <gired0329@yahoo.com>
2021-11-22 22:00:05 -0500

    added comment, edit, new.js files for added functionality

--- Main/public/login.js
+++ Main/public/login.js
@@ -24,36 +24,7 @@ event.preventDefault();
     }
   };
 
-
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
-  
-//     const username = document.querySelector('#registerUsername').value.trim();
-//     const email = document.querySelector('#registerEmail').value.trim();
-//     const password = document.querySelector('#registerPassword').value.trim();
-//     console.log(username,email,password)
-//     if (username && email && password) {
-//       const response = await fetch('/api/users', {
-//         method: 'POST',
-//         body: JSON.stringify({ username, email, password }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/');
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-  
-//   };
-
-//   document.addEventListener('DOMContentLoaded', () => {
     document
       .querySelector('#loginForm')
       .addEventListener('submit', loginFormHandler);
     
-  //   document
-  //     .querySelector('#registerFrom')
-  //     .addEventListener('submit', signupFormHandler);
-  // });
